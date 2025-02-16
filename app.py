c12a95f833e1202bf68867823662d2dfeffac025
kontelk <ktelkis@gmail.com>
2025-02-16 13:48:00 +0200

    minor changes

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+  """View for the Home page of the website."""
+  return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+  """View for the About page of the website."""
+  return render_template("about.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+  app.run(debug=True, host="0.0.0.0", port=3000)
